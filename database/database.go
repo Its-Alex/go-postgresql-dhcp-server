6fee48c5722e801cb1d2219e5e2f28b4449d8e3d
Alex M <me@itsalex.fr>
2018-05-28 17:17:45 +0200

    fix: fix typo in sqlx request

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq" // Used by sqlx as postgres driver
+	"github.com/sirupsen/logrus"
 )
 
 // Db variable used to store global database
@@ -10,14 +11,17 @@ var Db *sqlx.DB
 
 // Reservation structure in database
 type Reservation struct {
-	MaskSubnet string `json:"subnet_mask"`
-	IP         string `json:"ip"`
-	MAC        string `json:"mac"`
+	MaskSubnet string `db:"mask_subnet" json:"mask_subnet"`
+	MAC        string `db:"mac" json:"mac"`
+	IP         string `db:"ip" json:"ip"`
 }
 
 // GetReservationByMAC get a reservation from his mac address
 func GetReservationByMAC(mac string) Reservation {
 	var reservation Reservation
-	Db.Get(&reservation, "SELECT host(mask_subnet), text(mac), text(ip) FROM reservations WHERE mac == ?", mac)
+	err := Db.Get(&reservation, "SELECT * FROM reservations WHERE mac = $1", mac)
+	if err != nil {
+		logrus.Error(err)
+	}
 	return reservation
 }
