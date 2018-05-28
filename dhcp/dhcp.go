88983ccb3b55c8b6efe8d2b9d4ae7a3948cfc0b3
Alex M <me@itsalex.fr>
2018-05-28 13:08:34 +0200

    refacto: change struct split with package for better unit test

---
+++ dhcp/dhcp.go
@@ -0,0 +1,74 @@
+package dhcp
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/Its-Alex/dhcp4-reservation/database"
+	"github.com/krolaw/dhcp4"
+	"github.com/krolaw/dhcp4/conn"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+// Handler is struct containing server information
+type Handler struct {
+	ip            net.IP
+	leaseDuration time.Duration
+	options       dhcp4.Options
+}
+
+// ServeDHCP is a state machine for dhcp response
+func (h *Handler) ServeDHCP(p dhcp4.Packet, msgType dhcp4.MessageType, options dhcp4.Options) (d dhcp4.Packet) {
+	fmt.Println("NEW MESSAGE")
+	mac := p.CHAddr().String()
+
+	switch msgType {
+	case dhcp4.Discover:
+		reservation := database.GetReservationByMAC(mac)
+		if reservation.IP == "" {
+			logrus.Infof("Unknown server : %s", mac)
+			return nil
+		}
+		return dhcp4.ReplyPacket(p, dhcp4.Offer, h.ip, net.ParseIP(reservation.IP), h.leaseDuration,
+			h.options.SelectOrderOrAll(options[dhcp4.OptionParameterRequestList]))
+	case dhcp4.Request:
+		if server, ok := options[dhcp4.OptionServerIdentifier]; ok && !net.IP(server).Equal(h.ip) {
+			return nil
+		}
+		reqIP := net.IP(options[dhcp4.OptionRequestedIPAddress])
+		if reqIP == nil {
+			reqIP = net.IP(p.CIAddr())
+		}
+		reservation := database.GetReservationByMAC(mac)
+		if reservation.IP == "" {
+			logrus.Infof("Unknown server : %s", mac)
+			return dhcp4.ReplyPacket(p, dhcp4.NAK, h.ip, nil, 0, nil)
+		}
+		return dhcp4.ReplyPacket(p, dhcp4.ACK, h.ip, net.ParseIP(reservation.IP), h.leaseDuration,
+			h.options.SelectOrderOrAll(options[dhcp4.OptionParameterRequestList]))
+	}
+	return nil
+}
+
+// Start is used by cobra to launch programm
+func Start() {
+	conn, err := conn.NewUDP4FilterListener(viper.GetString("interface"), fmt.Sprintf("localhost:%s",
+		viper.GetString("port"),
+	))
+	if err != nil {
+		logrus.Fatal("NewUDP4FilterListener: ", err)
+	}
+	handler := &Handler{
+		ip:            net.ParseIP(viper.GetString("server_ip")),
+		leaseDuration: 2 * time.Hour,
+		options: dhcp4.Options{
+			dhcp4.OptionRouter:           []byte(viper.GetString("server_ip")),
+			dhcp4.OptionDomainNameServer: []byte(viper.GetString("server_ip")),
+			dhcp4.OptionBootFileName:     []byte("pxelinux.0"),
+		},
+	}
+	logrus.Info("dhcp4 start")
+	logrus.Fatal(dhcp4.Serve(conn, handler))
+}
---
+++ dhcp/dhcp_test.go
@@ -0,0 +1,54 @@
+package dhcp_test
+
+import (
+	"log"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Its-Alex/dhcp4-reservation/dhcp"
+	"github.com/krolaw/dhcp4"
+	"github.com/spf13/viper"
+)
+
+func TestMain(m *testing.M) {
+	log.Println("MAIN TEST")
+	viper.Set("port", "10000")
+	viper.Set("interface", "en0")
+	viper.Set("server_ip", "192.168.0.254")
+	go dhcp.Start()
+	time.Sleep(5 * time.Second)
+	os.Exit(m.Run())
+}
+
+func TestClientBroadcast(t *testing.T) {
+	log.Println("CLIENT BROACAST")
+	mac, err := net.ParseMAC("7b:31:15:6c:80:29")
+	if err != nil {
+		t.Error(err)
+	}
+
+	packet := dhcp4.RequestPacket(
+		dhcp4.Discover,
+		mac,
+		net.IP{192, 168, 0, 11},
+		dhcp4.Packet{},
+		true,
+		[]dhcp4.Option{},
+	)
+
+	conn, err := net.Dial("udp4", "localhost:10000")
+	if err != nil {
+		t.Error(err)
+	}
+
+	conn.Write(packet)
+	response := make([]byte, 4096)
+
+	_, err = conn.Read(response)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Error(response)
+}
